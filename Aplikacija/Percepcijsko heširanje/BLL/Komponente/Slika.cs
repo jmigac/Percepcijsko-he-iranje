def616d31e90dedc38a2f5dbda49373f1a1af3b3
Jurica Migač <jurica1.migac@gmail.com>
2019-11-23 23:31:43 +0100

    03 Izrada funkcije klase te komentiranje istih. #3
    Izrada funkcija za klasu Slika te komentiranje istih funkcija.

--- Aplikacija/Percepcijsko heširanje/BLL/Komponente/Slika.cs
+++ Aplikacija/Percepcijsko heširanje/BLL/Komponente/Slika.cs
@@ -11,6 +11,7 @@ namespace BLL.Komponente
         private String putanjaSlike;
         private String naslovOpenFileDialog;
         private String filterOpenFileDialog;
+        private String hashSlike;
 
         public Slika()
         {
@@ -18,18 +19,50 @@ namespace BLL.Komponente
             this.filterOpenFileDialog = "All Images | *.jpg; *.bmp; *.png";
 
         }
+        /// <summary>
+        /// Dohvaćanje izračunatog hasha slike.
+        /// </summary>
+        /// <returns>Hash slike</returns>
+        public String GetHashSlike()
+        {
+            return this.hashSlike;
+        }
+        /// <summary>
+        /// Postavljanje hash vrijednosti u obliku string vrijednosti.
+        /// </summary>
+        /// <param name="hash">Hash vrijednost</param>
+        public void SetHashSlike(String hash)
+        {
+            this.hashSlike = hash;
+        }
+        /// <summary>
+        /// Dohvaćanje putanje slike koja je učitana za odabrani objekt.
+        /// </summary>
+        /// <returns>Vraća putanja do slike na disku.</returns>
         public String GetPutanjaSlike()
         {
             return this.putanjaSlike;
         }
+        /// <summary>
+        /// Postavljanje putanje slike za određeni objekt koji se upravljaju svojstva određene slike.
+        /// </summary>
+        /// <param name="putanja"></param>
         public void SetPutanjaSlike(string putanja)
         {
             this.putanjaSlike = putanja;
         }
+        /// <summary>
+        /// Dohvaćanje vrijednosti o tipu dokumenta koji će se otvarati.
+        /// </summary>
+        /// <returns>Tip dokumenta</returns>
         public String GetNaslov()
         {
             return this.naslovOpenFileDialog;
         }
+        /// <summary>
+        /// Dohvaćanje vrijednosti filtera koje datoteke je moguće učitati u program.
+        /// </summary>
+        /// <returns>Filter za otvaranje datoteka.</returns>
         public String GetFilter()
         {
             return this.filterOpenFileDialog;
