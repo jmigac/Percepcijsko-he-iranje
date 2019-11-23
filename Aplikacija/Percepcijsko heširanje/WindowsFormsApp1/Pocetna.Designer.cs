5f1ee73504a8761458f0d4dbdcd0ec7835b00032
Jurica Migač <jurica1.migac@gmail.com>
2019-11-23 19:37:40 +0100

    02 Dodani gumbovi za heširanje slika. #2
    Dodani gumbovi na UI za heširanje slika te samim time imenovane metode koji će sve biti korištene za heširanje slika, a to su: Average Hash, Perceptual Hash, Wavelet Hash i Difference Hash

--- Aplikacija/Percepcijsko heširanje/WindowsFormsApp1/Pocetna.Designer.cs
+++ Aplikacija/Percepcijsko heširanje/WindowsFormsApp1/Pocetna.Designer.cs
@@ -34,6 +34,10 @@
             this.labelSlika2 = new System.Windows.Forms.Label();
             this.btnUcitajSliku1 = new System.Windows.Forms.Button();
             this.btnUcitajSliku2 = new System.Windows.Forms.Button();
+            this.btnAverageHash = new System.Windows.Forms.Button();
+            this.btnDifferenceHash = new System.Windows.Forms.Button();
+            this.btnPerceptualHash = new System.Windows.Forms.Button();
+            this.btnWaveletHash = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pbSlika1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbSlika2)).BeginInit();
             this.SuspendLayout();
@@ -90,11 +94,51 @@
             this.btnUcitajSliku2.Text = "Ucitaj";
             this.btnUcitajSliku2.UseVisualStyleBackColor = true;
             // 
+            // btnAverageHash
+            // 
+            this.btnAverageHash.Location = new System.Drawing.Point(63, 248);
+            this.btnAverageHash.Name = "btnAverageHash";
+            this.btnAverageHash.Size = new System.Drawing.Size(75, 59);
+            this.btnAverageHash.TabIndex = 4;
+            this.btnAverageHash.Text = "Average Hash";
+            this.btnAverageHash.UseVisualStyleBackColor = true;
+            // 
+            // btnDifferenceHash
+            // 
+            this.btnDifferenceHash.Location = new System.Drawing.Point(144, 248);
+            this.btnDifferenceHash.Name = "btnDifferenceHash";
+            this.btnDifferenceHash.Size = new System.Drawing.Size(75, 59);
+            this.btnDifferenceHash.TabIndex = 5;
+            this.btnDifferenceHash.Text = "Difference Hash";
+            this.btnDifferenceHash.UseVisualStyleBackColor = true;
+            // 
+            // btnPerceptualHash
+            // 
+            this.btnPerceptualHash.Location = new System.Drawing.Point(225, 248);
+            this.btnPerceptualHash.Name = "btnPerceptualHash";
+            this.btnPerceptualHash.Size = new System.Drawing.Size(75, 59);
+            this.btnPerceptualHash.TabIndex = 6;
+            this.btnPerceptualHash.Text = "Perception Hash";
+            this.btnPerceptualHash.UseVisualStyleBackColor = true;
+            // 
+            // btnWaveletHash
+            // 
+            this.btnWaveletHash.Location = new System.Drawing.Point(306, 248);
+            this.btnWaveletHash.Name = "btnWaveletHash";
+            this.btnWaveletHash.Size = new System.Drawing.Size(75, 59);
+            this.btnWaveletHash.TabIndex = 7;
+            this.btnWaveletHash.Text = "Wavelet Hash";
+            this.btnWaveletHash.UseVisualStyleBackColor = true;
+            // 
             // Pocetna
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(452, 450);
+            this.Controls.Add(this.btnWaveletHash);
+            this.Controls.Add(this.btnPerceptualHash);
+            this.Controls.Add(this.btnDifferenceHash);
+            this.Controls.Add(this.btnAverageHash);
             this.Controls.Add(this.btnUcitajSliku2);
             this.Controls.Add(this.btnUcitajSliku1);
             this.Controls.Add(this.labelSlika2);
@@ -118,6 +162,10 @@
         private System.Windows.Forms.Label labelSlika2;
         private System.Windows.Forms.Button btnUcitajSliku1;
         private System.Windows.Forms.Button btnUcitajSliku2;
+        private System.Windows.Forms.Button btnAverageHash;
+        private System.Windows.Forms.Button btnDifferenceHash;
+        private System.Windows.Forms.Button btnPerceptualHash;
+        private System.Windows.Forms.Button btnWaveletHash;
     }
 }
 
